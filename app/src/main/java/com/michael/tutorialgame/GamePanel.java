2f466ad6ae99ec569c66040d55c97d1a4291535c
michael <ortiz.d.michael@gmail.com>
2017-02-08 20:27:16 -0800

    More code for obstacles

--- app/src/main/java/com/michael/tutorialgame/GamePanel.java
+++ app/src/main/java/com/michael/tutorialgame/GamePanel.java
@@ -18,6 +18,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
     private RectPlayer player;
     private Point playerPoint;
+    private ObstacleManager obstacleManager;
 
     public GamePanel(Context context){
         super(context);
@@ -28,6 +29,8 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
         player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
         playerPoint = new Point(150, 150);
+
+        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
 
         setFocusable(true);
     }
@@ -47,7 +50,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
         boolean retry = true;
-        while(true) {
+        while(retry) {
             try {
                 thread.setRunning(false);
                 thread.join();
@@ -72,6 +75,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
     public void update() {
         player.update(playerPoint);
+        obstacleManager.update();
     }
 
     @Override
@@ -81,5 +85,6 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
         canvas.drawColor(Color.WHITE);
 
         player.draw(canvas);
+        obstacleManager.draw(canvas);
     }
 }
--- app/src/main/java/com/michael/tutorialgame/Obstacle.java
+++ app/src/main/java/com/michael/tutorialgame/Obstacle.java
@@ -10,11 +10,23 @@ import android.graphics.Rect;
 
 public class Obstacle implements GameObject{
     private Rect rectangle;
+    private Rect rectangle2;
     private int color;
 
-    public Obstacle(Rect rectangle, int color){
-        this.rectangle = rectangle;
+
+    public Rect getRectangle() {return rectangle;}
+
+    public void incrementY(float y) {
+        rectangle.top += y;
+        rectangle.bottom += y;
+        rectangle2.top += y;
+        rectangle2.bottom += y;
+    }
+
+    public Obstacle(int rectHeight, int color, int startX, int startY, int playerGap){
         this.color = color;
+        rectangle = new Rect(0, startY, startX, startY + rectHeight);
+        rectangle2 = new Rect(startX + playerGap, startY, Constants.SCREEN_WIDTH, startY + rectHeight);
     }
 
     public boolean playerCollide(RectPlayer player){
@@ -33,6 +45,7 @@ public class Obstacle implements GameObject{
         Paint paint = new Paint();
         paint.setColor(color);
         canvas.drawRect(rectangle, paint);
+        canvas.drawRect(rectangle2, paint);
     }
 
     @Override
