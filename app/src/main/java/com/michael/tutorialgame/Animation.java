2a6d98d2789364ab78f86b67fe0560a74b9ab0b5
michael <ortiz.d.michael@gmail.com>
2017-02-15 12:12:15 -0800

    small fixes

--- app/src/main/java/com/michael/tutorialgame/Animation.java
+++ app/src/main/java/com/michael/tutorialgame/Animation.java
@@ -4,7 +4,6 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.provider.Settings;
 
 /**
  * Created by micha on 2/12/2017.
